77b06e49f4a8192c44ba9037cf5a9bdfb68506bd
Mateo0207 <78557721+Mateo0207@users.noreply.github.com>
2021-09-27 10:24:00 -0500

    Signed-off-by: Mateo0207 <78557721+Mateo0207@users.noreply.github.com>

--- 01-03 - luz - 5cubos.js
+++ 01-03 - luz - 5cubos.js
@@ -43,21 +43,24 @@ function init() {
     scene.add(axes);
 
     Cubo = [];   // Definir un array unidimensional
-   // for(i=0; i<5;i++){
 
-    //}
+    for(var i=0; i<5; i++){
+    CuboA.push(cubo(4, 4, 4, 0xFFDD00, 'Standard', false));
+    CuboA[i].position.set(-4,i*4.2+4,4) 
+    
+}
+    //Cubo.push(cubo(4, 4, 4, 0xFFDD00, 'Standard', false)); 
+    //Cubo.push(cubo(4, 4, 4, 0xFF0000, 'Standard', false)); 
+    //Cubo.push(cubo(4, 4, 4, 0x00FFFF, 'Standard', false)); 
+    //Cubo.push(cubo(4, 4, 4, 800080, 'Standard', false)); 
+    //Cubo.push(cubo(4, 4, 4, 0xFFFFFF, 'Standard', false));
 
-    Cubo.push(cubo(4, 4, 4, 0xFFDD00, 'Standard', false)); 
-    Cubo.push(cubo(4, 4, 4, 0xFF0000, 'Standard', false)); 
-    Cubo.push(cubo(4, 4, 4, 0x00FFFF, 'Standard', false)); 
-    Cubo.push(cubo(4, 4, 4, 800080, 'Standard', false)); 
-    Cubo.push(cubo(4, 4, 4, 0xFFFFFF, 'Standard', false));
-
-    Cubo[0].position.set(0, 2, 0);
-    Cubo[1].position.set(0, 6, 0);
-    Cubo[2].position.set(0, 10, 0);
-    Cubo[3].position.set(0, 14, 0);
-    Cubo[4].position.set(0, 18, 0);
+   // Cubo[0].position.set(0, 2, 0);
+    //Cubo[1].position.set(0, 6, 0);
+    //Cubo[2].position.set(0, 10, 0);
+    //Cubo[3].position.set(0, 14, 0);
+    //Cubo[4].position.set(0, 18, 0);
+    
 
     //Luz (requerida para el material MeshLambertMaterial)
     light = new THREE.PointLight(0xFFFF00); //  Luz proveniente de un punto en el espacio, 
